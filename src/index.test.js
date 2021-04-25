dbe9acba6256acf1cb91d3c53749bc475ce6d9ca
Kate Morris <kate.morris.new@gmail.com>
2021-04-25 10:36:05 +0100

    Attempted to add jest test

---
+++ src/index.test.js
@@ -0,0 +1,49 @@
+import { fireEvent, getByText } from '@testing-library/dom'
+import '@testing-library/jest-dom/extend-expect'
+import { JSDOM } from 'jsdom'
+import fs from 'fs'
+import path from 'path'
+
+const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
+
+let dom
+let container
+describe('index.html', () => {
+    beforeEach(() => {
+    //   // Constructing a new JSDOM with this option is the key
+    //   // to getting the code in the script tag to execute.
+    //   // This is indeed dangerous and should only be done with trusted content.
+    //   // https://github.com/jsdom/jsdom#executing-scripts
+      // dom = new JSDOM(html, { runScripts: 'dangerously' })
+      dom = new JSDOM('', { url: 'https://localhost' });
+      container = dom.window.document.body;
+    })
+    it('renders a heading element', () => {
+      console.log("Well what do we have?",container.querySelector('h1'));
+        expect(container.querySelector('h1')).not.toBeNull()
+        // expect(getByText(container, 'Walk In Their Shoes')).toBeInTheDocument()
+        // expect(getByText('button')).not.toBeDisabled();
+
+      })
+    
+      // it('renders a button element', () => {
+      //   expect(container.querySelector('button')).not.toBeNull()
+      //   expect(getByText(container, 'Click me for a terrible pun')).toBeInTheDocument()
+      // })
+    
+      // it('renders a new paragraph via JavaScript when the button is clicked', async () => {
+      //   const button = getByText(container, 'Click me for a terrible pun')
+        
+      //   fireEvent.click(button)
+      //   let generatedParagraphs = container.querySelectorAll('#pun-container p')
+      //   expect(generatedParagraphs.length).toBe(1)
+    
+      //   fireEvent.click(button)
+      //   generatedParagraphs = container.querySelectorAll('#pun-container p')
+      //   expect(generatedParagraphs.length).toBe(2)
+    
+      //   fireEvent.click(button)
+      //   generatedParagraphs = container.querySelectorAll('#pun-container p')
+      //   expect(generatedParagraphs.length).toBe(3)
+      // })
+    })
